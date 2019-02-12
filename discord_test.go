44b8a7893edc0ce43b7028b32f64fdc3d8ead0c1
Connor <fox@nerdfox.me>
2019-02-11 23:56:18 -0700

    Fixed tests

--- discord_test.go
+++ discord_test.go
@@ -6,132 +6,53 @@ import (
 	"testing"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/globalsign/mgo/bson"
 )
 
-type testUser struct {
-	ID      string
-	guildID string
-	bot     bool
-}
-
-var testUsers = []testUser{
-	testUser{ //User1
-		ID:      "123",
-		guildID: "1234",
-		bot:     false,
-	},
-	testUser{ //User2
-		ID:      "456",
-		guildID: "1234",
-		bot:     false,
-	},
-	testUser{ //User1 Diffirent Guild
-		ID:      "123",
-		guildID: "4567",
-		bot:     false,
-	},
-}
-
-var testGuilds = []*discordgo.Guild{
-	&discordgo.Guild{
-		ID: "1234",
-		Presences: []*discordgo.Presence{
-			&discordgo.Presence{
-				User: &discordgo.User{
-					ID: testUsers[0].ID,
-				},
-				Game: &discordgo.Game{
-					Name: "Test",
-				},
-			},
-			&discordgo.Presence{
-				User: &discordgo.User{
-					ID: testUsers[1].ID,
-				},
+func createFakePresences(usersToLink []*discordgo.User, gameNameStr string) []*discordgo.Presence {
+	presenceArr := make([]*discordgo.Presence, 0)
+	for i := range usersToLink {
+		if gameNameStr == "" {
+			presenceArr = append(presenceArr, &discordgo.Presence{
+				User: usersToLink[i],
 				Game: nil,
-			},
-		},
-		Members: []*discordgo.Member{
-			&discordgo.Member{
-				User: &discordgo.User{
-					ID: testUsers[0].ID,
-				},
-			},
-			&discordgo.Member{
-				User: &discordgo.User{
-					ID: testUsers[1].ID,
-				},
-			},
-		},
-	},
-	&discordgo.Guild{
-		ID: "5678",
-		Presences: []*discordgo.Presence{
-			&discordgo.Presence{
-				User: &discordgo.User{
-					ID: testUsers[0].ID,
-				},
-				Game: &discordgo.Game{
-					Name: "Test",
-				},
-			},
-			&discordgo.Presence{
-				User: &discordgo.User{
-					ID: testUsers[1].ID,
-				},
-				Game: nil,
-			},
-		},
-		Members: []*discordgo.Member{
-			&discordgo.Member{
-				User: &discordgo.User{
-					ID: testUsers[0].ID,
-				},
-			},
-			&discordgo.Member{
-				User: &discordgo.User{
-					ID: testUsers[1].ID,
-				},
-			},
-		},
-	},
-}
-
-func createFakePresence(userToLink *discordgo.User, gameNameStr string) *discordgo.Presence {
-	if gameNameStr == "" {
-		return &discordgo.Presence{
-			User: userToLink,
-			Game: nil,
+			})
 		}
+		presenceArr = append(presenceArr, &discordgo.Presence{
+			User: usersToLink[i],
+			Game: &discordgo.Game{
+				Name: gameNameStr,
+			},
+		})
 	}
-	return &discordgo.Presence{
-		User: userToLink,
-		Game: &discordgo.Game{
-			Name: gameNameStr,
-		},
-	}
+	return presenceArr
 }
 
-func createFakeGuild(presences []*discordgo.Presence) *discordgo.Guild {
+func createFakeGuild(presences []*discordgo.Presence, users []*discordgo.User) *discordgo.Guild {
 	randID := rand.Intn(1000000)
+	guildMembers := make([]*discordgo.Member, 0)
+	for i := range users {
+		guildMembers = append(guildMembers, &discordgo.Member{
+			User: users[i],
+		})
+	}
 	return &discordgo.Guild{
-		ID: strconv.Itoa(randID),
+		ID:        strconv.Itoa(randID),
+		Members:   guildMembers,
+		Presences: presences,
 	}
 }
 
-func createFakeMember(isBot bool) *discordgo.Member {
-	randID := rand.Intn(1000000)
-	return &discordgo.Member{
-		User: &discordgo.User{
-			ID: strconv.Itoa(randID),
-		},
+func createFakeMembers(isBot bool, howMany int) []*discordgo.User {
+	memberArr := make([]*discordgo.User, 0)
+	for i := 0; i < howMany; i++ {
+		randID := rand.Intn(1000000)
+		memberArr = append(memberArr, &discordgo.User{
+			ID:  strconv.Itoa(randID),
+			Bot: isBot,
+		})
 	}
-}
-
-func TestCreate(t *testing.T) {
-	m1 := createFakeMember(false)
-	g1 := createFakeGuild()
-	p1 := createFakePresence(m1)
+	return memberArr
 }
 
 func TestSetUpDB(t *testing.T) {
@@ -139,93 +60,88 @@ func TestSetUpDB(t *testing.T) {
 		_, testDB := setUpDB("localhost/testing")
 		db = testDB
 	}
-}
-
-func TestAddUser(t *testing.T) {
-	TestSetUpDB(t)
-	testUser := testUsers[0]
-	addDiscordUser(testUser.ID, testUser.guildID, testUser.bot)
-
-	if discordUsers[testUser.ID].userID != testUser.ID {
-		t.Error("User ID is not equal to Test ID")
-	}
-	if discordUsers[testUser.ID].mainGuild != testUser.guildID {
-		t.Error("Guild ID is not equal to Test ID")
-	}
-	delete(discordUsers, testUser.ID)
-}
-
-func TestAddSameUser(t *testing.T) {
-	TestSetUpDB(t)
-	testUser := testUsers[0]
-	testUserSame := testUsers[0]
-	addDiscordUser(testUser.ID, testUser.guildID, testUser.bot)
-	addDiscordUser(testUserSame.ID, testUserSame.guildID, testUserSame.bot)
-
-	if len(discordUsers) > 1 {
-		t.Error("Length is GT 1")
-		t.FailNow()
-	}
-	delete(discordUsers, testUser.ID)
-}
-
-func TestAddUserDiffirentGuild(t *testing.T) {
-	TestSetUpDB(t)
-	testUser := testUsers[0]
-	testUserGuild := testUsers[2]
-	addDiscordUser(testUser.ID, testUser.guildID, testUser.bot)
-	addDiscordUser(testUserGuild.ID, testUserGuild.guildID, testUserGuild.bot)
-
-	if len(discordUsers[testUser.ID].otherGuilds) == 0 {
-		t.Error("otherGuild length is equal to 0")
-	}
-	delete(discordUsers, testUser.ID)
-	delete(discordUsers, testUserGuild.ID)
+	db.removeAll("settings", bson.M{})
+	db.removeAll("gamestats", bson.M{})
 }
 
 func TestAddGuild(t *testing.T) {
 	TestSetUpDB(t)
-	addDiscordGuild(testGuilds[0])
+	pMembers := createFakeMembers(false, 3)
+	nMembers := createFakeMembers(false, 3)
+	pPresences := createFakePresences(pMembers, "Test")
+	nPresences := createFakePresences(nMembers, "")
+	members := append(pMembers, nMembers...)
+	presences := append(pPresences, nPresences...)
+	guild := createFakeGuild(presences, members)
+
+	newMembers := createFakeMembers(false, 3)
+	newPresences := createFakePresences(newMembers, "Test2")
+	newMembers = append(newMembers, pMembers...)
+	newPresences = append(newPresences, pPresences...)
+	guild2 := createFakeGuild(newPresences, newMembers)
+
+	addDiscordGuild(guild)
 
 	if len(discordUsers) == 0 {
-		t.Error("Length of users is 0 ")
-		t.Error(discordUsers)
-	}
-	if discordUsers[testUsers[0].ID].isPlaying == false {
-		t.Error("Is playing is false User1")
-	}
-	if discordUsers[testUsers[0].ID].currentGame == "" {
-		t.Error("Game name is wrong User1")
+		t.Error("Length is Equal to 0")
 	}
 
-	if discordUsers[testUsers[1].ID].currentGame != "" {
-		t.Error("Game name is wrong User2")
-		t.Error(discordUsers[testUsers[2].ID].currentGame)
+	for i := range members {
+		if presences[i].Game != nil {
+			if discordUsers[members[i].ID].currentGame != presences[i].Game.Name {
+				t.Error("Game name is not equal")
+				t.Error("Expected: " + presences[i].Game.Name + "Got: " + discordUsers[members[i].ID].currentGame)
+			}
+		} else if discordUsers[members[i].ID].currentGame != "" {
+			t.Error("Game is not equal")
+			t.Error("Expected: " + "Got: " + discordUsers[members[i].ID].currentGame)
+		}
 	}
-	delete(discordUsers, testUsers[0].ID)
-	delete(discordUsers, testUsers[1].ID)
+
+	addDiscordGuild(guild2)
+
+	if len(discordUsers) != (len(members) + 3) {
+		t.Error("Error adding guild with some same users")
+	}
+
+	for _, item := range discordUsers {
+		delete(discordUsers, item.userID)
+	}
+	if len(discordUsers) != 0 {
+		t.Error("Clearing Failed")
+	}
 }
 
-func TestAddGuildSameUsers(t *testing.T) {
+func TestAddSingleUser(t *testing.T) {
 	TestSetUpDB(t)
-	addDiscordGuild(testGuilds[0])
-	addDiscordGuild(testGuilds[1])
+	members := createFakeMembers(false, 3)
+	presences := createFakePresences(members, "Test2")
+	guild := createFakeGuild(presences, members)
+	addDiscordGuild(guild)
 
-	if len(discordUsers) == len(testGuilds[0].Members) {
-		t.Error("Length of users is 0 ")
-		t.Error(discordUsers)
-	}
-	if discordUsers[testUsers[0].ID].isPlaying == false {
-		t.Error("Is playing is false User1")
-	}
-	if discordUsers[testUsers[0].ID].currentGame == "" {
-		t.Error("Game name is wrong User1")
+	memberToAdd := createFakeMembers(false, 1)
+	addDiscordUser(memberToAdd[0].ID, guild.ID, false)
+
+	if len(discordUsers) != 4 {
+		t.Error("Discord Users Length Incorrent")
+		t.Error(len(discordUsers))
 	}
 
-	if discordUsers[testUsers[1].ID].currentGame != "" {
-		t.Error("Game name is wrong User2")
-		t.Error(discordUsers[testUsers[2].ID].currentGame)
+	addDiscordUser(memberToAdd[0].ID, guild.ID, false)
+
+	if len(discordUsers) != 4 {
+		t.Error("Discord Users Length Incorrent")
+		t.Error(len(discordUsers))
 	}
-	delete(discordUsers, testUsers[0].ID)
-	delete(discordUsers, testUsers[1].ID)
+
+	for _, item := range discordUsers {
+		delete(discordUsers, item.userID)
+	}
+	if len(discordUsers) != 0 {
+		t.Error("Clearing Failed")
+	}
+}
+
+func TestRemoveUser(t *testing.T) {
+
 }
